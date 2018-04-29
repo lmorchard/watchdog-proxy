f6966bfbf08bc3418c70b95e7fff7a9d261e57ac
Les Orchard <me@lmorchard.com>
2018-04-28 22:46:27 -0400

    Play some more with dynamodb and s3

--- functions/enqueue.js
+++ functions/enqueue.js
@@ -1,43 +1,68 @@
 "use strict";
 
-const util = require("util");
-const AWS = require("aws-sdk");
+const { promisifyMethods } = require("../lib/utils");
+const { s3, sqs, dynamodb, lockClient } = require("../lib/aws");
 
-const QUEUE_NAME = process.env.QueueName;
+const { CONFIG, QUEUE_NAME, LOCKS_TABLE, CONTENT_BUCKET } = process.env;
 
-const promisify = (object, methods) =>
-  methods.reduce(
-    (out, name) => ({
-      ...out,
-      [name]: util.promisify(object[name].bind(object))
-    }),
-    { object }
-  );
-
-const sqs = promisify(new AWS.SQS({apiVersion: '2012-11-05'}), [
-  "getQueueUrl",
-  "sendMessage"
-]);
-
-module.exports.handler = async function (event, context, callback) {
-  console.log("QUEUE NAME", QUEUE_NAME, process.env);
-  // console.log("request: " + JSON.stringify(event));
-
+module.exports.handler = async function(
+  {
+    resource,
+    path,
+    body,
+    headers,
+    queryStringParameters,
+    requestContext: {
+      stage,
+      requestTimeEpoch,
+      httpMethod,
+      requestId
+    }
+  },
+  context,
+  callback
+) {
   var responseCode = 200;
   var responseBody = {
-    input: event
+    resource,
+    path,
+    body,
+    headers,
+    queryStringParameters,
+    stage,
+    requestTimeEpoch,
+    httpMethod,
+    requestId
   };
+
+  try {
+    const lock = promisifyMethods(await lockClient.acquireLock("rateLimit"), [
+      "release"
+    ]);
+
+    responseBody.lockResult = "SUCCESS";
+
+    const result = await s3.putObject({
+      Bucket: CONTENT_BUCKET,
+      Key: requestId,
+      Body: "THIS IS A TEST"
+    });
+    responseBody.s3Result = result;
+
+    await lock.release();
+  } catch (err) {
+    responseBody.lockResult = "ERR " + err;
+  }
 
   try {
     const { QueueUrl } = await sqs.getQueueUrl({ QueueName: QUEUE_NAME });
     const { MessageId } = await sqs.sendMessage({
       MessageBody: JSON.stringify({
         nowish: Date.now(),
-        event
+        requestId
       }),
       QueueUrl
     });
-    console.log("data:", MessageId);
     responseBody.sqsResult = "SUCCESS " + MessageId;
   } catch (err) {
     responseBody.sqsResult = "ERR " + err;
